d15c2080f3240ce2fb9a6a09d27ba8439fa19777
Gabriel S. Duarte <gsdo1@aluno.ifal.edu.br>
2026-05-22 16:33:52 -0300

    Implementação de rotas para Usuário

---
+++ backend/routes/UsuarioRoutes.js
@@ -0,0 +1,18 @@
+import { UsuarioController } from "../controllers/UsuarioController.js";
+import express from 'express';
+
+const controller = new UsuarioController();
+
+const router = express.Router();
+
+// ROTAS DE USUÁRIO
+router.post('/usuarios', controller.cadastrar);
+
+router.get('/usuarios/:id', controller.buscar);
+
+router.put('/usuarios/:id', controller.atualizar);
+
+router.delete('/usuarios/:id', controller.deletar);
+
+// EXPORT
+export default router;
