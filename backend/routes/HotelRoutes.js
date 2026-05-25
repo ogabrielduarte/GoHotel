9e290ccc007abff046c212b0263bbe75a8211ad9
Gabriel S. Duarte <gsdo1@aluno.ifal.edu.br>
2026-05-25 18:20:40 -0300

    Add rotas de Hotel

---
+++ backend/routes/HotelRoutes.js
@@ -0,0 +1,18 @@
+import { HotelController } from "../controllers/HotelController.js";
+import express from 'express';
+
+const controller = new HotelController();
+
+const router = express.Router();
+
+// ROTAS DE USUÁRIO
+router.post('/hoteis', controller.cadastrar);
+
+router.get('/hoteis/:id', controller.buscar);
+
+router.put('/hoteis/:id', controller.atualizar);
+
+router.delete('/hoteis/:id', controller.deletar);
+
+// EXPORT
+export default router;
