0538a6030e3e78d58eb5cacb98817dbe81052276
Gabriel S. Duarte <gsdo1@aluno.ifal.edu.br>
2026-05-26 22:32:36 -0300

    Add ReservaController

---
+++ backend/controllers/ReservaController.js
@@ -0,0 +1,150 @@
+import { Reserva } from "../models/negocio/Reserva.js";
+import { ReservaDAO } from "../models/persistencia/ReservaDAO.js";
+
+export class ReservaController {
+
+    async cadastrar(req, res) {
+
+        try {
+
+            const reserva = new Reserva(req.body);
+
+            const dao = new ReservaDAO();
+
+            const id = await dao.cadastrar(reserva);
+
+            res.status(201).json({
+                id
+            });
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+    async buscar(req, res) {
+
+        try {
+
+            const id = Number(req.params.id);
+
+            if (!id) {
+                return res.status(400).json({
+                    erro: 'A reserva não existe'
+                });
+            }
+
+            const dao = new ReservaDAO();
+
+            const reserva = await dao.buscaPorId(id);
+
+            res.status(200).json(reserva);
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+    async listarReservasUsuario(req, res) {
+
+        try {
+
+            const idUsuario = Number(req.params.id);
+
+            if (!idUsuario) {
+                return res.status(400).json({
+                    erro: 'O usuário não existe'
+                });
+            }
+
+            const dao = new ReservaDAO();
+
+            const reservas = await dao.listarReservasUsuario(idUsuario);
+
+            res.status(200).json(reservas);
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+    async atualizar(req, res) {
+
+        try {
+
+            const id = Number(req.params.id);
+
+            if (!id) {
+                return res.status(400).json({
+                    erro: 'A reserva não existe'
+                });
+            }
+
+            const dados = req.body;
+
+            if (Object.keys(dados).length === 0) {
+                return res.status(400).json({
+                    erro: 'Não há campos atualizados'
+                });
+            }
+
+            const dao = new ReservaDAO();
+
+            const update = await dao.atualizar(dados, id);
+
+            res.status(200).json(update);
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+    async deletar(req, res) {
+
+        try {
+
+            const id = Number(req.params.id);
+
+            if (!id) {
+                return res.status(400).json({
+                    erro: 'A reserva não existe'
+                });
+            }
+
+            const dao = new ReservaDAO();
+
+            const deletado = await dao.deletar(id);
+
+            res.status(200).json(deletado);
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+}
