27d2aa9d9b42a09a7218cccffa8ff8c5730298df
GABRIEL SANTOS DUARTE DE OLIVEIRA <2024327117@MACEIO.IFAL>
2026-05-28 13:46:26 -0300

    correção de módulos em app.js

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
-// IMPORTAÇÃO DAS ROTAS
+// ROTAS
 import usuarioRoutes from './routes/UsuarioRoutes.js';
 import hotelRoutes from './routes/HotelRoutes.js';
 import telefoneRoutes from './routes/TelefonesRoutes.js';
 import reservaRoutes from './routes/ReservaRoutes.js';
 
-// carregar .env
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-})
 
 // app.use()
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-// app.use() para rotas
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// app.use() rotas
 app.use(usuarioRoutes);
-
 app.use(hotelRoutes);
-
 app.use(telefoneRoutes);
-
 app.use(reservaRoutes);
 
 export default app;
