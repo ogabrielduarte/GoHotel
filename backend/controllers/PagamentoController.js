592f8c87bf65283bbe840b214198ed0e45f7ba78
Gabriel S. Duarte <gsdo1@aluno.ifal.edu.br>
2026-05-31 23:30:11 -0300

    add pagamento controller

---
+++ backend/controllers/PagamentoController.js
@@ -0,0 +1,122 @@
+import { Pagamento } from '../models/negocio/Pagamento.js';
+import { PagamentoDAO } from '../models/persistencia/PagamentoDAO.js';
+
+export class PagamentoController {
+
+    async cadastrar(req, res) {
+
+        try {
+
+            const pagamento = new Pagamento(req.body);
+
+            const dao = new PagamentoDAO();
+
+            const id = await dao.cadastrar(pagamento);
+
+            res.status(201).json({
+                id
+            });
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+    async buscar(req, res) {
+
+        try {
+
+            const id = Number(req.params.id);
+
+            if (!id) {
+                return res.status(400).json({
+                    erro: 'Pagamento não existe'
+                });
+            }
+
+            const dao = new PagamentoDAO();
+
+            const pagamento = await dao.buscaPorId(id);
+
+            res.status(200).json(pagamento);
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+    async atualizar(req, res) {
+
+        try {
+
+            const id = Number(req.params.id);
+
+            if (!id) {
+                return res.status(400).json({
+                    erro: 'Pagamento não existe'
+                });
+            }
+
+            const dados = req.body;
+
+            if (Object.keys(dados).length === 0) {
+                return res.status(400).json({
+                    erro: 'Não há campos atualizados'
+                });
+            }
+
+            const dao = new PagamentoDAO();
+
+            const update = await dao.atualizar(dados, id);
+
+            res.status(200).json(update);
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+    async deletar(req, res) {
+
+        try {
+
+            const id = Number(req.params.id);
+
+            if (!id) {
+                return res.status(400).json({
+                    erro: 'Pagamento não existe'
+                });
+            }
+
+            const dao = new PagamentoDAO();
+
+            const deletado = await dao.deletar(id);
+
+            res.status(200).json(deletado);
+
+        } catch (e) {
+
+            res.status(400).json({
+                erro: e.message || e
+            });
+
+        }
+
+    }
+
+}
