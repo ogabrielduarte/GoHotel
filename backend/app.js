be18b2b36b162bb5ce846617c6f8f79775ef4132
ogabrielduarte <gabriel.sduarte.oli@gmail.com>
2026-06-05 14:10:00 -0300

    feat: enable CORS for all origins

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 // ROTAS
 import usuarioRoutes from './routes/UsuarioRoutes.js';
@@ -12,6 +13,8 @@ const app = express();
 
 // app.use()
 app.use(express.json());
+
+app.use(cors()); // provavelmente será trocado por uma configuração mais específica de CORS, na 3001 ou 4000, para o frontend. por ora, deixamos aberto para facilitar os testes.
 
 app.use(express.urlencoded({ extended: true }));
 
