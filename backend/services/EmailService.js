6ba2adcc04116eef75dfaf0755bb30b81ba70b50
ogabrielduarte <gabriel.sduarte.oli@gmail.com>
2026-06-06 22:22:38 -0300

    Add e-mail sending service to GoHotel

---
+++ backend/services/EmailService.js
@@ -0,0 +1,32 @@
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+require('dotenv').config({ path: resolve(__dirname, '../.env') });
+
+import nodemailer from 'nodemailer';
+
+console.log("MAIL:", process.env.MAIL_USERNAME);
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD
+    }
+});
+
+export async function sendEmail(destinatario, assunto, mensagem) {
+    return await transporter.sendMail({
+        from: process.env.MAIL_USERNAME,
+        to: destinatario,
+        subject: assunto,
+        html: mensagem
+    });
+}
